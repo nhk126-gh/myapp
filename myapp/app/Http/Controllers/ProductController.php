dc8bfbcd73293061720ad6a8a8eea9023153e07c
hrk126 <hrk.nrs.26@gmail.com>
2022-02-16 19:56:29 +0900

    5th

--- myapp/app/Http/Controllers/ProductController.php
+++ myapp/app/Http/Controllers/ProductController.php
@@ -7,7 +7,10 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
+    //dig()にてコピーした配列を入れるための配列
     public $connect_arr;
+    //search_parent()にて親品番のProduct_idを入れるための配列
+    public $parent_arr_id = array();
 
     //全検索
     public function findall(Request $request)
@@ -16,7 +19,7 @@ class ProductController extends Controller
         return view('findall', ['items' => $items]);
     }
 
-    //品番検索
+    //部分検索
     public function search(Request $request)
     {
         $items = Product::where('address', 'like', '%' . $request->input . '%')
@@ -29,42 +32,38 @@ class ProductController extends Controller
         return view('search', ['items' => $items]);
     }
     
-    //前工程検索
+    //工程検索
     public function process(Request $request)
     {
-        // 配列初期化
-        $this->connect_arr = array();
-        $changed_arr = array();
-
         //基準を1点取出す
         $item = Product::find($request->input);
-
-        // 基準が見つかれば前工程検索
+        // 基準が見つかれば次の処理
         // 無ければNotFoundを返す
         if (isset($item)) {
-            $items = $item->before;
-            $this->dig($items, [$item]);
-        }else{
+            //親品番確認
+            $this->search_parent($item);
+
+            if ($this->parent_arr_id[0] == $item->id){
+                //親品番がなかった場合
+                $result = $this->make_process($item);
+                return view('process', ['items' => $result]);
+            } else {
+                //親品番があれば配列をforで回す
+                //検索品番のidを渡す
+                $id = $item->id;
+                //重複削除
+                $parent_arr_unique_id = array_unique($this->parent_arr_id);
+                $results = array();
+                foreach ($parent_arr_unique_id as $parent_id){
+                    $item = Product::find($parent_id);
+                    $result = $this->make_process($item);
+                    array_push($results, $result);
+                }
+                return view('processmany', ['results' => $results, 'id' => $id]);
+            }
+        } else {
             return view('process');
         }
-
-        // 配列をview用に変換
-        array_push($changed_arr, $this->connect_arr[0]);
-        for ($i=1; $i<count($this->connect_arr); $i++){
-            $buf = $this->change_array($this->connect_arr[$i-1], $this->connect_arr[$i]);
-            array_push($changed_arr, $buf);
-        }
-
-        //配列の要素数をそろえる
-        $counts = array_map(function ($item) {
-                    return count($item);
-                }, $changed_arr);
-        $max_count = max($counts);
-        $dist = array_map(function ($item) use($max_count) {
-                    return array_pad($item, $max_count, 'x');
-                }, $changed_arr);
-
-        return view('process', ['items' => $dist]);
     }
 
     /**
@@ -116,6 +115,60 @@ class ProductController extends Controller
         }
         
         return $arr2;
+    }
+
+    /**
+     * 1つの製品から前工程を取得する
+     *
+     * @param  App\Models\Product  $item
+     * 
+     * @return array
+     */
+    public function make_process($item)
+    {
+        // 配列初期化
+        $this->connect_arr = array();
+
+        $items = $item->before;
+        $this->dig($items, [$item]);
+        // 配列をview用に変換
+        $changed_arr = array();
+        array_push($changed_arr, $this->connect_arr[0]);
+        for ($i=1; $i<count($this->connect_arr); $i++){
+            $buf = $this->change_array($this->connect_arr[$i-1], $this->connect_arr[$i]);
+            array_push($changed_arr, $buf);
+        }
+
+        //配列の要素数をそろえる
+        $counts = array_map(function ($item) {
+                    return count($item);
+                }, $changed_arr);
+        $max_count = max($counts);
+        $dist = array_map(function ($item) use($max_count) {
+                    return array_pad($item, $max_count, 'x');
+                }, $changed_arr);
+        
+        return $dist;
+    }
+
+    /**
+     * 親品番有無確認
+     *
+     * @param  App\Models\Product  $item
+     * 
+     * @return void
+     */
+    public function search_parent($item)
+    {
+        $items = $item->after;
+        if ($items->isEmpty()){
+            array_push($this->parent_arr_id, $item->id);
+            return;
+        } else {
+            foreach ($items as $item) {
+                $this->search_parent($item);
+            }
+        }
     }
 
 }
